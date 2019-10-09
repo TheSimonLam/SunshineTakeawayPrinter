61624c931b825bca71d890d96497814110fefbfc
simon.lam <simon.lam@moneysupermarket.com>
2019-10-09 14:19:23 +0100

    done it

---
+++ src/components/Category.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="type-container" @click="toggleShowItems">{{category.name}}</div>
+    <div v-if="showItems">
+      <div class="item-square-container" @click="addToOrder(item)" v-for="item in category.items" v-bind:key="item.number">
+        <div class="item item-number">{{item.number}}</div>
+        <div class="item item-name">{{item.name}}</div>
+        <div class="item item-price">£{{item.price}}</div>
+        <div class="item item-allergens">{{item.allergens}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'category',
+  props: ['category'],
+  data: function(){
+    return {
+      showItems: false
+    }
+  },
+  methods: {
+    toggleShowItems(){
+      this.showItems = !this.showItems
+    },
+    addToOrder(item){
+      this.$store.commit("addItemToOrder", item);
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '../css/global.scss';
+
+  .type-container{
+    width: 100%;
+    display: inline-block;
+    background: lightblue;
+    padding: 25px 0;
+    margin: 10px 0;
+  }
+
+  .item-square-container{
+      height: 150px;
+      width: 150px;
+      background: lightblue;
+      margin: 10px;
+      display: inline-block;
+      padding: 10px;
+      text-align: left;
+      border-radius: 5px;
+      vertical-align: top;
+    }
+
+    .item {
+      padding: 5px 0;
+    }
+
+    .item-number{
+
+    }
+
+    .item-name{
+      font-size: 1.3em;
+      font-weight: bold;
+    }
+
+    .item-price{
+      
+    }
+
+    .item-allergens{
+      
+    }
+</style>
